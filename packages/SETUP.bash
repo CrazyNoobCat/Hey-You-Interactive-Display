36252844a3f387fb8b2842e578de29e1f9f43f4d
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-02-01 10:46:19 +0000

    Updated to work with Linux as well as Cygwin

--- packages/SETUP.bash
+++ packages/SETUP.bash
@@ -1,7 +1,16 @@
 
-pushd nodejs-windows/
-export PATH=$PWD:$PATH
-popd
+os=$(uname -s)
 
-echo "Updated PATH to include nodejs-windows"
+if [ "x$os" = "xLinux" ] ; then
+    export PATH=$PWD/node-v16.13.2-linux-x64/bin:$PATH
+elif [ "x${os%%_*}" = "xCygwin" ] ; then    
+    export PATH=$PWD/node-v16.13.1-win-x64:$PATH
+else
+    echo "Unrecognized Operating System: $os" >&2
+    echo "Failed to update PATH to include node, npm and npx"
+    return 
+fi
+
+
+echo "Updated PATH to include node, npm and npx"
 
