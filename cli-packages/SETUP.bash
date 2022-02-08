73ec4a64b0cb48bdd41f9f282f6ccfb1b0a29bfe
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-02-08 02:00:15 +0000

    Refactored to have more shared code

--- cli-packages/SETUP.bash
+++ cli-packages/SETUP.bash
@@ -1,16 +1,35 @@
 
+nodejs_package=node-v16.13.2
+
+
 os=$(uname -s)
 
 if [[ "x$os" =~ "xLinux" ]] ; then
-    export PATH=$PWD/node-v16.13.2-linux-x64/bin:$PATH
-elif [[ "x${os%%_*}" =~ "xCYGWIN" ]] ; then    
-    export PATH=$PWD/node-v16.13.1-win-x64:$PATH
+    nodejs_package=$nodejs_package-linux-x64
+
+elif [[ "x${os%%_*}" =~ "xCYGWIN" ]] ; then
+    nodejs_package=$nodejs_package-win-x64
 else
     echo "Unrecognized Operating System: $os" >&2
     echo "Failed to update PATH to include node, npm and npx"
     return 
 fi
 
+if [ ! -d $nodejs_package ] ; then
+    echo "" >&2
+    echo "Failed to find directory:" >&2
+    echo "  $nodejs_package" >&2
+    echo "" >&2
+    echo "Have you untarred/unzipped the NodeJS binary distribution?" >&2
+    echo "For example:" >&2
+    echo "  tar xvf $nodejs_package.tar.xz" >&2
+    echo "Or:" >&2
+    echo "  unzip $nodejs_package.zip" >&2
+    echo "" >&2
+    return
+fi
+    
+export PATH=$PWD/$nodejs_package/bin:$PATH
 
 echo "Updated PATH to include node, npm and npx"
 
