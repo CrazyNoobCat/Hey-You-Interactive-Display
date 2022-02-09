8b91346f18bd8536281323cf06c1ead1c2931842
David Bainbridge <davidb@waikato.ac.nz>
2022-02-09 11:03:19 +0000

    Tweaks after testing

--- heyyou-start-server.sh
+++ heyyou-start-server.sh
@@ -3,7 +3,10 @@
 full_progname=`pwd`/${BASH_SOURCE}
 heyyou_home=${full_progname%/*}
 
+echo "Logging output to:"
+echo "  heyyou-server.log"
+
 cd "$heyyou_home" \
-    && ./heyyou-setup.bash \
+    && . ./heyyou-setup.bash \
     && node index.js </dev/null >heyyou-server.log 2>&1
 
