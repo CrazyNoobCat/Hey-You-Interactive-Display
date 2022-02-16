6d0e09b601a4750c9e1aad22a7f6cdf3cbaa1ac2
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-02-16 00:16:54 +0000

    Repositioning of ChromeCast debugging lines (already commented out) to be after chromecastContext and chromecastSettings are defined

--- public/receiver.js
+++ public/receiver.js
@@ -1,24 +1,3 @@
-//const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
-
-/*castDebugLogger.loggerLevelByEvents = {
-    'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
-    'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
-  }*/
-
-// Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
-//castDebugLogger.setEnabled(true);
-
-// Show debug overlay
-//castDebugLogger.showDebugLogs(true);
-
-//castDebugLogger.debug('before',chromecastSettings.disableIdleTimeout);
-
-//castDebugLogger.debug('useragent',ua);
-
-
-// Based on:
-//  https://modernweb.com/using-media-queries-in-javascript/
-//var mq = window.matchMedia('@media tv');
 
 var ua = navigator.userAgent;
 
@@ -26,6 +5,25 @@ if(ua.includes("CrKey")) {
     const chromecastContext = cast.framework.CastReceiverContext.getInstance(); 
     const chromecastSettings = new cast.framework.CastReceiverOptions();
 
+    /*
+    const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+
+    castDebugLogger.loggerLevelByEvents = {
+	'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+	'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+    }
+    
+    // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+    castDebugLogger.setEnabled(true);
+
+    // Show debug overlay
+    castDebugLogger.showDebugLogs(true);
+
+    castDebugLogger.debug('before',chromecastSettings.disableIdleTimeout);
+    castDebugLogger.debug('useragent',ua);
+    */
+    
+    
     chromecastSettings.disableIdleTimeout = true;
     
     chromecastContext.start(chromecastSettings);
