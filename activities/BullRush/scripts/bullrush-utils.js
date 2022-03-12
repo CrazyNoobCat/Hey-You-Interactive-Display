9cc502e9f33407e6dbc4cda9994883d971cab12c
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-03-12 22:36:13 +0000

    Further tidy up

--- activities/BullRush/scripts/bullrush-utils.js
+++ activities/BullRush/scripts/bullrush-utils.js
@@ -1,11 +1,11 @@
 function  drawRunner(ctx,xOrg,yOrg,radius,fillStyle,textLabel)
 {
-    // Circle for nontaggers
+    // Draw a circle for runners (i.e. the non-taggers)
     ctx.lineWidth = 1;
     ctx.strokesStyle = "#FFFFFF";
 
     ctx.beginPath();
-    ctx.arc(xOrg, yOrg, radius, 0, Math.PI * 2);
+    ctx.arc(xOrg, yOrg, radius, 0, Math.PI*2);
     ctx.fillStyle = fillStyle;
     ctx.stroke();
     ctx.fill();
@@ -24,19 +24,11 @@ function  drawTagger(ctx,xOrg,yOrg,radius,fillStyle,textLabel)
     ctx.lineWidth = 1;
     ctx.strokesStyle = "#FFFFFF";
 
-    var spikes = 16;
-    var rot = Math.PI / 2 * 3;
-    var step = Math.PI / spikes;
+    var numSpikes = 16;
+    var rot    = Math.PI / 2 * 3;
+    var step   = Math.PI / numSpikes;
 
-    // When radius was hardwired to 18
-    // console.log("*** drawTagger() radius = " + radius);
-    // var outerRadius = radius - 0.5;
-    // var innerRadius = radius - 4;
-
-    //var outerRadius = radius * 35/36.0;
-    //var innerRadius = radius * 14/18.0;
-
-    // Change to work of 20
+    // Propotions based off a radius of 20 (in normalized space, -0.5, and 4.0)
     var outerRadius = radius * 39/40.0;
     var innerRadius = radius * 16/20.0;
 
@@ -45,7 +37,7 @@ function  drawTagger(ctx,xOrg,yOrg,radius,fillStyle,textLabel)
     
     ctx.beginPath();
     ctx.moveTo(xOrg, yOrg - outerRadius)
-    for (var i = 0; i < spikes; i++) {
+    for (var i=0; i<numSpikes; i++) {
 	x = xOrg + Math.cos(rot) * outerRadius;
 	y = yOrg + Math.sin(rot) * outerRadius;
 	ctx.lineTo(x, y)
@@ -57,7 +49,7 @@ function  drawTagger(ctx,xOrg,yOrg,radius,fillStyle,textLabel)
 	rot += step
     }
     
-    ctx.lineTo(xOrg, yOrg - outerRadius);
+    ctx.lineTo(xOrg, yOrg-outerRadius);
     ctx.closePath();
     ctx.fillStyle = fillStyle;
     ctx.stroke();
