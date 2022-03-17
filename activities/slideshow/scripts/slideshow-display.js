c9fb9f17e60224c77ed9126d94c4be7ee170cf66
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-03-17 09:47:18 +0000

    Slide scaled to fit display area (with 90% reduction); QRcode moved to bottom and shaded background added

--- activities/slideshow/scripts/slideshow-display.js
+++ activities/slideshow/scripts/slideshow-display.js
@@ -1,12 +1,3 @@
-var slideDir = "/smoke-and-mirrors-sampler";
-var slideExt = ".PNG";
-
-var currentSlidePos = 0;
-var overlaySlidePos = 1;
-var currentSlideFS  = currentSlidePos + 1;
-var overlaySlideFS  = overlaySlidePos + 1;
-      
-var numSlides = 15;      
       
 function doSlideTransition(transitionType)
 {
@@ -86,37 +77,3 @@ function startPrevSlide()
     setTimeout(() => { doSlideTransition("fromright") }, 100);
 }
 
-$(document).ready(function() {
-    // Set up first slide 
-    var $currentSlideImg = $('#currentslide-img');
-    var $overlaySlideImg = $('#overlayslide-img');
-    
-    $currentSlideImg.attr("src", slideDir + "/Slide" + currentSlideFS + slideExt);
-    $overlaySlideImg.attr("src", slideDir + "/Slide" + overlaySlideFS + slideExt);
-    
-    $currentSlideImg.on("load", function() {
-	$currentSlideImg.fadeIn(1000);
-	
-	if (numSlides > 1) {
-	    // Get the next slide set up as the overlay, but need to wait until fadeIn done
-	    setTimeout(() => { $overlaySlideImg.css("display", "block"); }, 1000);		  
-	    
-	}	  	      
-    });
-
-    // https://stackoverflow.com/questions/6186454/is-there-a-callback-on-completion-of-a-css3-animation        
-    //var element = document.getElementById("currentslide-li");
-    
-    //element.addEventListener("webkitAnimationEnd", startNextSlide, false);
-    //element.addEventListener("animationend",       startNextSlide, false);
-    //element.addEventListener("oanimationend",      startNextSlide, false);
-    //element.addEventListener("MSAnimationEnd",     startNextSlide, false);
-
-    // Consider changing to jQuery way of doing this?
-    //
-    $("#currentslide-li").bind('animationend oanimationend webkitAnimationEnd MSAnimationEnd', function() { 
-	startNextSlide() 
-    });
-
-    doSlideTransition("fromright");        
-});
