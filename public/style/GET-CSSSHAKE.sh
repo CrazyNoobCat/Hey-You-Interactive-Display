a5c2ad6e69535711a32db8d286596673d4303610
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-03-17 04:02:25 +0000

    Self-contained version of CSS-Shake

---
+++ public/style/GET-CSSSHAKE.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ""
+echo "****"
+echo "Using wget to download full CSS version"
+echo "****"
+echo ""
+
+wget "https://csshake.surge.sh/csshake.css"
+
+echo ""
+echo "****"
+echo "Using wget to download minified CSS version"
+echo "****"
+echo ""
+
+wget "https://csshake.surge.sh/csshake.min.css"
+
