f456644a3441164d0d5c29eb7651e036949e8d6b
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-03-17 02:26:40 +0000

    Code tidyup, then some refactoring of names: base => current, and always having 'slide' in combo with 'current' or 'overlay'

--- activities/SlideshowTest/scripts/slideshow-display.js
+++ activities/SlideshowTest/scripts/slideshow-display.js
@@ -1,3 +1,12 @@
+var slideDir = "/smoke-and-mirrors-sampler";
+var slideExt = ".PNG";
+
+var currentSlidePos = 0;
+var overlaySlidePos = 1;
+var currentSlideFS  = currentSlidePos + 1;
+var overlaySlideFS  = overlaySlidePos + 1;
+      
+var numSlides = 15;      
       
 function doSlideTransition(transitionType)
 {
@@ -5,11 +14,11 @@ function doSlideTransition(transitionType)
     
     var root = document.querySelector(':root');
     //root.style.setProperty('--animation-time', '2s');
-    root.style.setProperty('--animation-baseslide', transitionType+'-baseslide');
-    root.style.setProperty('--animation-overlay',   transitionType+'-overlay');
+    root.style.setProperty('--animation-currentslide', transitionType+'-currentslide');
+    root.style.setProperty('--animation-overlayslide', transitionType+'-overlayslide');
     
-    document.getElementById("baseslide-li").classList.add("baseslide-animation");
-    document.getElementById("overlay-li").classList.add("overlay-animation");
+    document.getElementById("currentslide-li").classList.add("currentslide-animation");
+    document.getElementById("overlayslide-li").classList.add("overlayslide-animation");
     
     var progressBar = document.getElementById("progress-bar");
     progressBar.style.display = "block";
@@ -21,43 +30,29 @@ function doSlideTransition(transitionType)
 function slideAnimationReset()
 {
     //console.log("slideAnimationReset() called with optCallback = " + optCallback);
-    /*
-      document.getElementById("baseslide-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
-      
-      currentSlidePos = (currentSlidePos + 1) % maxSlides;
-      overlaySlidePos = (overlaySlidePos + 1) % maxSlides;
-      
-      currentSlideFS = currentSlidePos + 1;
-      overlaySlideFS = overlaySlidePos + 1;
-      
-      //if (overlaySlidePos < maxSlides) {
-      document.getElementById("overlay-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
-      //}
-      */
     
-    document.getElementById("baseslide-li").classList.remove("baseslide-animation");
-    document.getElementById("overlay-li").classList.remove("overlay-animation");
+    document.getElementById("currentslide-li").classList.remove("currentslide-animation");
+    document.getElementById("overlayslide-li").classList.remove("overlayslide-animation");
     document.getElementById("progress-bar").classList.remove("progress-bar-animate");
 }
 
-
-
-var startNextSlide = function () {
-    // console.log("*** startNextSlide");
+function startNextSlide() {
+    
+    //console.log("*** startNextSlide()");
     
     slideAnimationReset();
     
     // Overlay slide becomes the new current slide
-    document.getElementById("baseslide-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
+    document.getElementById("currentslide-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
     
     // Advance the counter on by +1
-    currentSlidePos = (currentSlidePos + 1) % maxSlides;
-    overlaySlidePos = (overlaySlidePos + 1) % maxSlides;	    
+    currentSlidePos = (currentSlidePos + 1) % numSlides;
+    overlaySlidePos = (overlaySlidePos + 1) % numSlides;	    
     currentSlideFS = currentSlidePos + 1;
     overlaySlideFS = overlaySlidePos + 1;
     
     // Set up the new overlay slide
-    document.getElementById("overlay-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
+    document.getElementById("overlayslide-img").src = slideDir + "/Slide" + overlaySlideFS + slideExt;
     
     
     // Use setTimeout so initiating next slide occurs outside of the event handler for AnimationEnd
@@ -69,19 +64,40 @@ var startNextSlide = function () {
     setTimeout(() => { doSlideTransition("fromright") }, 100); 
 };
 
+function startPrevSlide()
+{
+    //console.log("*** startPrevSlide()");
+    
+    slideAnimationReset();
+
+    // Wind everything back by one
+    currentSlidePos = (numSlides + currentSlidePos - 1) % numSlides;
+    overlaySlidePos = (numSlides + overlaySlidePos - 1) % numSlides;
+    
+    currentSlideFS = currentSlidePos + 1; // Slide num on file system starts at 1, not 0
+    overlaySlideFS = overlaySlidePos + 1; // ditto
+	      
+    var $currentSlideImg = $('#currentslide-img');
+    var $overlaySlideImg = $('#overlayslide-img');
+    
+    $currentSlideImg.attr("src", slideDir + "/Slide" + currentSlideFS + slideExt);
+    $overlaySlideImg.attr("src", slideDir + "/Slide" + overlaySlideFS + slideExt);
+    
+    setTimeout(() => { doSlideTransition("fromright") }, 100);
+}
 
 $(document).ready(function() {
     // Set up first slide 
-    var $baseSlideImg = $('#baseslide-img');
-    var $overlaySlideImg = $('#overlay-img');
+    var $currentSlideImg = $('#currentslide-img');
+    var $overlaySlideImg = $('#overlayslide-img');
     
-    $baseSlideImg.attr("src", slideDir + "/Slide" + currentSlideFS + slideExt);
+    $currentSlideImg.attr("src", slideDir + "/Slide" + currentSlideFS + slideExt);
     $overlaySlideImg.attr("src", slideDir + "/Slide" + overlaySlideFS + slideExt);
     
-    $baseSlideImg.on("load", function() {
-	$baseSlideImg.fadeIn(1000);
+    $currentSlideImg.on("load", function() {
+	$currentSlideImg.fadeIn(1000);
 	
-	if (maxSlides > 1) {
+	if (numSlides > 1) {
 	    // Get the next slide set up as the overlay, but need to wait until fadeIn done
 	    setTimeout(() => { $overlaySlideImg.css("display", "block"); }, 1000);		  
 	    
@@ -89,18 +105,18 @@ $(document).ready(function() {
     });
 
     // https://stackoverflow.com/questions/6186454/is-there-a-callback-on-completion-of-a-css3-animation        
-    var element = document.getElementById("baseslide-li");
+    //var element = document.getElementById("currentslide-li");
     
-    element.addEventListener("webkitAnimationEnd", startNextSlide, false);
-    element.addEventListener("animationend",       startNextSlide, false);
-    element.addEventListener("oanimationend",      startNextSlide, false);
-    element.addEventListener("MSAnimationEnd",     startNextSlide, false);
+    //element.addEventListener("webkitAnimationEnd", startNextSlide, false);
+    //element.addEventListener("animationend",       startNextSlide, false);
+    //element.addEventListener("oanimationend",      startNextSlide, false);
+    //element.addEventListener("MSAnimationEnd",     startNextSlide, false);
 
     // Consider changing to jQuery way of doing this?
     //
-    // $("#baseslide-li").bind('animationend oanimationend webkitAnimationEnd MSAnimationEnd', function() { 
-    //  startNextSlide() 
-    // });
+    $("#currentslide-li").bind('animationend oanimationend webkitAnimationEnd MSAnimationEnd', function() { 
+	startNextSlide() 
+    });
 
     doSlideTransition("fromright");        
 });
